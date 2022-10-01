f2d04f31ed10aff75125e7b1facff94bb146952f
fygure <92011471+fygure@users.noreply.github.com>
2022-10-01 04:20:31 -0500

    first commit, setup argument manager

---
+++ solution.cpp
@@ -0,0 +1,32 @@
+// 2436 (fall 2022) GA1
+// Solution program that solves the problem listed in the pdf
+//=======================================================================//
+#include <iostream>
+#include <fstream>
+#include "ArgumentManager.h"
+//=======================================================================//
+using namespace std;
+
+
+//=======================================================================//
+int main(int argc, char *argv[]) {
+
+    // I/O and test case handler
+    // ArgumentManager am(argc, argv);
+    // ifstream inFS(am.get("input"));
+    // ifstream cmdFS(am.get("command"));
+    // ofstream outFS(am.get("output"));
+
+    ifstream inFS("input11.txt");
+
+
+    inFS.close();
+
+
+
+
+
+    
+    return 0;
+}
+//=======================================================================//
